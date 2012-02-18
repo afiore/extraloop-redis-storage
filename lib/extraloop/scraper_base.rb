bd505b6bc1b15abdbd79262c9472526019bc246d
Andrea Fiore <and@inventati.org>
2012-02-18 14:04:40 +0000

    Fixes scope problem in #set_storage method

--- lib/extraloop/scraper_base.rb
+++ lib/extraloop/scraper_base.rb
@@ -1,32 +1,33 @@
 module ExtraLoop
   class ScraperBase
-    def set_storage(model=nil, title = "", &block)
-      log_session!(title) if @request_count === 0
-  
-      if model
-        @model = model.respond_to?(:new) ?
-          model : ExtraLoop::Storage::DatasetFactory.new(model.to_sym, @extractor_args.map(&:first)).get_class
+    def set_storage(title, model=nil)
+      log_session!(title)
+
+      model ||= self.class.to_s.gsub(/Scraper/,'Data').downcase.to_sym
+
+      @model = model_klass = model && model.respond_to?(:new) || ExtraLoop::Storage::DatasetFactory.new(model.to_sym, @extractor_args.map(&:first)).get_class
+
+      on :data do |results|
+        # TODO: find a way to avoid calling send in the scraper object
+        results = results.map { |result| model_klass && @scraper.send(:instanciate_model, result) || result }
+        binding.pry
+        #TODO: will this block given work?
+        block_given? && yield(results) || results.each { |result| result.save if result.respond_to?(:save) && result.id.nil? }
       end
-  
-      on(:data, proc { |results|
-        results = results.map { |result| @model && instanciate_model(result) || result }
-        block && block.call(results) || results.each(&:save)
-      })
     end
-  
+
     protected
-  
     # Creates a scraping session
-  
     def log_session!(title="")
       @session ||= ExtraLoop::Storage::ScrapingSession.create :title => title
     end
-  
+
     # Converts extracted records into instances of the dataset model specified as the first argument
     # of #set_storage
-  
+
     def instanciate_model(record)
-      attrs = {:session => @session }.merge(record.respond_to?(:marshal_dump) ? record.marshal_dump : record)
+      record_hash = record.respond_to?(:marshal_dump) ? record.marshal_dump : record
+      attrs = {:session_id => @session.id }.merge(record_hash)
       @model.new(attrs)
     end
   end
