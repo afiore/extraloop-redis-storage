087c42ffde4c2186d545f7a2b1c7f55e93876846
Andrea Fiore <andrea.giulio.fiore@gmail.com>
2012-04-03 15:09:20 +0100

    removed Object#try method

--- lib/extraloop/support.rb
+++ lib/extraloop/support.rb
@@ -13,12 +13,6 @@ class Hash
   end
 end
 
-class Object
-  def try(method, *args)
-    send method, *args if respond_to? method
-  end
-end
-
 class String
   def capitalize_first
     self[0].upcase + self[1, size]
